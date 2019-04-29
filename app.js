3bfc78610077fecd415dc53a876506b43a73d8f0
Jaakko <jaakko.ihanamaki@outlook.com>
2019-04-29 12:39:38 +0300

    Add commenting to app.js

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
+//Declare Express and Request variables
 const express = require('express')
 const request = require('request')
 const app = express()
 
-
+//Set express to host public directory
 app.use(express.static('public'))
 
+//Fetch job entires from Duunitori API and send them to front-end
 request('https://duunitori.fi/api/v1/jobentries?search=harjoittelija&format=json', function(error, response, body) {
   console.error('error: ', error)
   console.log('statusCode: ', response && response.statusCode)
@@ -14,8 +16,7 @@ request('https://duunitori.fi/api/v1/jobentries?search=harjoittelija&format=json
   })
 })
 
+//Set Express to listen to Heroku default port
 app.listen(process.env.PORT, function() {
-  console.log('App listening on 5000')
+  console.log('App listening')
 })
-
-//process.env.PORT
